808842c535c6d0d8dbb0e29d2c3365cd8f872f04
mbtipersonality <156853367+mbtipersonality@users.noreply.github.com>
2025-11-21 02:00:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
     
     const search2 = JSON.parse(localStorage.getItem('nsfw'))
 
-    if(search2){
-      const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
-      const newNsfw = search2.filter(item => item !== randSearch)
-      localStorage.setItem('nsfw', JSON.stringify(newNsfw))
-      window.location = `https://www.reddit.com/r/${randSearch}`
-    }
-    else {
-      localStorage.setItem('nsfw', JSON.stringify(search1))
-    }
+   
+    const randSearch = search2[Math.floor(Math.random() * (search2.length - 1 + 1) + 1 )]
+    const newNsfw = search2.filter(item => item !== randSearch)
+    localStorage.setItem('nsfw', JSON.stringify(newNsfw))
+    window.location = `https://www.reddit.com/r/${randSearch}`
+    
+    
+   
+    
   }
   return (
     <div className='App' onClick={handleClick}>
